2d897afd83ab897d1efe08b9c71f1b443f520626
ViktorMun <v.mun@yahoo.com>
2018-03-10 12:50:41 +0100

    Frontend completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Board from './containers/Board'
+import CreateGameButton from './components/CreateGameButton'
+import Progress from './components/Progress'
+import Finished from './components/Finished'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="title">0hh1</h1>
+
+        <div className="actions">
+          <CreateGameButton />
+        </div>
+        <Progress />
+        <Board />
+        <Finished />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/Square.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import './Square.css'
+
+
+
+export default class Square extends PureComponent {
+
+  render() {
+
+    return (
+      <div className="Square">import './Board.css'
+      </div>
+    )
+  }
+}
