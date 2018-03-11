fd3596a9447cb81a88d9435ba6cf76695888590c
ViktorMun <v.mun@yahoo.com>
2018-03-11 10:59:55 +0100

    Link create game with backend

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,17 +1,42 @@
-// src/actions/game.js
-
-import { CREATE_GAME } from './types'
+import request from 'superagent'
+//import { CREATE_GAME } from './types'
 import { MOVE } from './types'
 import { fillBoard } from '../lib/game'
 
+const baseUrl = 'http://localhost:3030/games'
+
+
+
+// export const createGame = (rows = 6) => {
+//   const [board, locked] = fillBoard(rows)
+//   return {
+//     type: CREATE_GAME,
+//     payload: {
+//       board,
+//       locked
+//     }
+//   }
+// }
+
+export const CREATE_GAME = 'CREATE_GAME'
+
 export const createGame = (rows = 6) => {
-  const [board, locked] = fillBoard(rows)
-  return {
-    type: CREATE_GAME,
-    payload: {
-      board,
-      locked
-    }
+  return (dispatch) => {
+    request
+      .post(`${baseUrl}`)
+      .then((response) => {
+        const [board, locked] = response.body
+        dispatch({
+          type: 'CREATE_GAME',
+          payload: {
+                board,
+                locked
+              }
+        })
+      })
+      .catch((error) => {
+        console.error('Something went wrong!', error)
+      })
   }
 }
 
